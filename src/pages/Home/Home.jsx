0ca6c1ff96460f527ff4de26a8ac4f231551f0ee
ProgSomel <somelahmed88@gmail.com>
2024-04-27 00:31:23 +0600

    Tourists Spots Section done

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,18 @@
+import { useLoaderData } from "react-router-dom";
 import Banner from "./Banner/Banner";
+import SpotCard from "./SpotCard/SpotCard";
 
 const Home = () => {
+    const spots = useLoaderData();
+
     return (
         <div>
             <Banner></Banner>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 max-w-6xl mx-auto px-4 md:px-2 gap-12">
+               {
+                spots?.map((spot)=> <SpotCard key={spot._id} spot={spot}></SpotCard>)
+               }
+            </div>
         </div>
     );
 };
---
+++ src/pages/Home/SpotCard/SpotCard.jsx
@@ -0,0 +1,66 @@
+import { PropTypes } from "prop-types";
+import { CiClock2 } from "react-icons/ci";
+import { BsPeople } from "react-icons/bs";
+import { FaArrowRightLong } from "react-icons/fa6";
+import { TiWeatherCloudy } from "react-icons/ti";
+
+const SpotCard = ({ spot }) => {
+  const {
+    imageUrl,
+    touristsSpotName,
+    countryName,
+    location,
+    shortDescription,
+    averageCost,
+    seasonality,
+    travelTime,
+    totalVisitorsPerYear,
+    userEmail,
+    userName,
+  } = spot;
+
+  return (
+    <div className="">
+      <div className="card h-[460px]  bg-base-100 shadow-xl">
+        <div className="relative">
+        <figure>
+
+          <img className="hover:scale-125  rounded-tr-md rounded-tl-md"
+            src={imageUrl}
+            alt="Shoes"
+          />
+        </figure>
+        <div className="absolute top-4 left-2">
+
+            <button className=" bg-green-500 px-12 rounded-md text-white font-bold py-1 flex items-center gap-2"><CiClock2 />{travelTime}</button>
+        </div>
+        </div>
+        <div className="card-body">
+          <div className="flex items-center gap-4 mb-5">
+          <h2 className="card-title text-2xl">{touristsSpotName}</h2>
+          <div className="flex items-center gap-1">
+            <p className="text-blue-400"><TiWeatherCloudy /></p>
+            <p className="text-blue-500">{seasonality}</p>
+          </div>
+          </div>
+          <div className="mb-3">
+            <p className="flex items-center gap-2"><span className="text-orange-400 font-bold "><BsPeople /></span><span className="font-extralight">{totalVisitorsPerYear}</span></p>
+          </div>
+          <div className="flex items-center gap-4">
+            <button className="border border-green-300 px-5 text-green-500 font-bold rounded-md flex items-center gap-2 hover:bg-green-900">VIEW DETAILS <span className="text-green-300"><FaArrowRightLong /></span></button>
+            <div>
+                <p className="">Average Cost</p>
+            <p className="text-green-600 font-bold">{averageCost}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+SpotCard.propTypes = {
+  spot: PropTypes.object,
+};
+
+export default SpotCard;
